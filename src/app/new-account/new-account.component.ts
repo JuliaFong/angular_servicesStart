078a0bb73c6e5e60971315b376549fd888a8e691
Julia Fong <Juliamzfong@gmail.com>
2020-04-05 16:28:07 -0500

    Add cross component communication

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -4,6 +4,7 @@ import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
 
+
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
@@ -12,12 +13,15 @@ import { AccountsService } from '../accounts.service';
 })
 export class NewAccountComponent {
   constructor(private loggingService: LoggingService,
-    private accountsService: AccountsService){
-
-    }
+              private accountsService: AccountsService){
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
   
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus)
     // this.loggingService.logStatusChange(accountStatus);
+  
   }
 }
