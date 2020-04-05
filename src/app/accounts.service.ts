05df36ce3c7e25b2956519017ed443bd363c1fc5
Julia Fong <Juliamzfong@gmail.com>
2020-04-05 16:22:51 -0500

    Add Injectable

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,5 +1,7 @@
 import { LoggingService } from './logging.service';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AccountsService {
     accounts = [
         {
@@ -19,8 +21,10 @@ export class AccountsService {
 
       addAccount(name: string, status: string) {
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status)
       }
     updateStatus(id: number, status: string) {
         this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status)
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -16,8 +18,9 @@ import { NewAccountComponent } from './new-account/new-account.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
   ],
-  providers: [NewAccountComponent],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
